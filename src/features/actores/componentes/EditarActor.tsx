97cfa4c8df3e4c2c5069146647425fc9b61503bc
Jager1026 <jandressanchez26@gmail.com>
2025-08-28 10:34:57 -0500

    Creacion Actor form added

--- src/features/actores/componentes/EditarActor.tsx
+++ src/features/actores/componentes/EditarActor.tsx
@@ -1,12 +1,35 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router"
+import type ActorCreacion from "../modelos/ActorCreacion.model";
+import FormularioActor from "./FormularioActor";
+import Cargando from "../../../componentes/Cargando";
+import type { SubmitHandler } from "react-hook-form";
 
-export default function EditarActor(){
-    const {id} = useParams(); 
-    return(
+export default function EditarActor() {
+
+    const { id } = useParams();
+
+    const [modelo, setModelo] = useState<ActorCreacion | undefined>(undefined);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setModelo({ nombre: 'Tom ' + id, fechaNacimiento: '2022-11-23' })
+        }, 1000);
+
+        return () => clearTimeout(timerId)
+    }, [id])
+
+    const onSubmit: SubmitHandler<ActorCreacion> = async (data) => {
+        console.log('Editando actor...');
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        console.log(data)
+    }
+
+    return (
         <>
             <h3>Editar Actor</h3>
-            <p>El id es {id}</p>
+            {modelo ? <FormularioActor modelo={modelo} onSubmit={onSubmit} /> : <Cargando />}
         </>
-       
+
     )
 }
