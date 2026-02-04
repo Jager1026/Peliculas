ff354f6b730169922b1f3a10e19e35d8e0caa0ec
Jager1026 <jandressanchez26@gmail.com>
2026-02-04 11:21:05 -0500

    Actores index updated, now paginacion is working and actor names are displayed

--- src/features/actores/componentes/IndiceActores.tsx
+++ src/features/actores/componentes/IndiceActores.tsx
@@ -1,12 +1,66 @@
 import { useNavigate } from "react-router";
 import Boton from "../../../componentes/Boton";
+import { useActores } from "../hooks/useActores";
+import Cargando from "../../../componentes/Cargando";
+import Paginacion from "../../../componentes/Paginacion";
+import ListadoGenerico from "../../../componentes/ListadoGenerico";
+import confirmar from "../../../utils/confirmar";
 
 export default function IndiceActores() {
     const navigate = useNavigate();
+
+    const { cargando, pagina, recordsPorPagina, cantidadTotalRegistros, setPagina,
+        setRecordsPorPagina, actores } = useActores();
     return (
         <>
             <h3>Actores</h3>
-            <Boton onClick={() => navigate('/actores/crear')}>Crear Actor</Boton>
+            <div>
+                <Boton onClick={() => navigate('/actores/crear')}>Crear Actor</Boton>
+            </div>
+
+
+            {cargando ? <Cargando /> :
+                <div className="mt-4">
+                    <div className="mb-2">
+                        <Paginacion paginaActual={pagina} registrosPorPagina={recordsPorPagina}
+                            cantidadTotalRegistros={cantidadTotalRegistros}
+                            registrosPorPaginaOpciones={[5, 10, 50]}
+                            onCambioPaginacion={(pagina, recordsPorPagina) => {
+                                setPagina(pagina);
+                                setRecordsPorPagina(recordsPorPagina);
+                            }
+                            } />
+                    </div>
+
+                    <ListadoGenerico listado={actores}>
+                        <table className="table table-hover align-middle shadow-sm border rounded overflow-hidden">
+                            <thead className="table-light">
+                                <tr>
+                                    <th scope="col">Nombre</th>
+                                    <th scope="col" className="text-end">Acciones</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                {actores?.map(actor => <tr key={actor.id}>
+                                    <td>{actor.nombre}</td>
+                                    <td className="text-end">
+                                        <Boton className="btn btn-sm btn-outline-primary me-2"
+                                            onClick={() => navigate(`/actores/editar/${actor.id}`)}>
+                                            <i className="bi bi-pencil me-1"></i> Editar
+                                        </Boton>
+
+                                        <Boton className="btn btn-sm btn-outline-danger me-2"
+                                            onClick={() => confirmar(() => { })}>
+                                            <i className="bi bi-trash"></i> Borrar
+                                        </Boton>
+
+                                    </td>
+                                </tr>)}
+                            </tbody>
+                        </table>
+                    </ListadoGenerico>
+                </div>}
         </>
     )
 }
