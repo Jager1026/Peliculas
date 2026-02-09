d40eafaeb7000f7fdbba0c2f007c77cdfd4e5ae1
Jager1026 <jandressanchez26@gmail.com>
2026-02-09 11:47:23 -0500

    formatearFecha added and now user can edit Actores from Frontend

--- src/features/actores/componentes/EditarActor.tsx
+++ src/features/actores/componentes/EditarActor.tsx
@@ -1,34 +1,50 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router"
+import { useNavigate, useParams } from "react-router"
 import type ActorCreacion from "../modelos/ActorCreacion.model";
 import FormularioActor from "./FormularioActor";
 import Cargando from "../../../componentes/Cargando";
 import type { SubmitHandler } from "react-hook-form";
+import clienteAPI from "../../../api/clienteAxios";
+import formatearFecha from "../../../utils/FormatearFecha";
+import type Actor from "../modelos/Actor.model"
+import { extraerErrores } from "../../../utils/extraerErrores";
+import { AxiosError } from "axios";
 
 export default function EditarActor() {
 
     const { id } = useParams();
-
+    const navigate = useNavigate();
     const [modelo, setModelo] = useState<ActorCreacion | undefined>(undefined);
+    const [errores, setErrores] = useState<string[]>([])
 
     useEffect(() => {
-        const timerId = setTimeout(() => {
-            setModelo({ nombre: 'Tom ' + id, fechaNacimiento: '2022-11-23', foto:'https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Elizabeth_Olsen_at_the_2024_Toronto_International_Film_Festival_3_%28cropped%29.jpg/800px-Elizabeth_Olsen_at_the_2024_Toronto_International_Film_Festival_3_%28cropped%29.jpg' })
-        }, 1000);
+        clienteAPI.get<Actor>(`/actores/${id}`).then(res => {
+            const actor = res.data;
+            const actorCreacion : ActorCreacion = {
+                nombre: actor.nombre,
+                fechaNacimiento: formatearFecha(actor.fechaNacimiento),
+                foto : actor.foto
+            };
 
-        return () => clearTimeout(timerId)
+            setModelo(actorCreacion);
+        })
     }, [id])
 
     const onSubmit: SubmitHandler<ActorCreacion> = async (data) => {
-        console.log('Editando actor...');
-        await new Promise(resolve => setTimeout(resolve, 2000));
-        console.log(data)
+        try{
+            await clienteAPI.putForm(`actores/${id}`, data);
+            navigate ('/sctores');
+        }
+        catch(err){
+            const errores = extraerErrores(err as AxiosError);
+            setErrores(errores);
+        }
     }
 
     return (
         <>
             <h3>Editar Actor</h3>
-            {modelo ? <FormularioActor errores={[]} modelo={modelo} onSubmit={onSubmit} /> : <Cargando />}
+            {modelo ? <FormularioActor errores={errores} modelo={modelo} onSubmit={onSubmit} /> : <Cargando />}
         </>
 
     )
